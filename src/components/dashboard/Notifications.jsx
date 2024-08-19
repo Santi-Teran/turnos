be188728ed9dc75f88defb148199639162d2d3d4
Santi-Teran <santiagoteranmatias@gmail.com>
2024-08-19 16:26:37 -0300

    notifications

--- src/components/dashboard/Notifications.jsx
+++ src/components/dashboard/Notifications.jsx
@@ -1,41 +1,41 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as signalR from '@microsoft/signalr';
 
-const Notifications = () => {
-  const [notifications, setNotifications] = useState([]);
+const Notifications = ({ token }) => {
+    const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    const connection = new signalR.HubConnectionBuilder()
-      .withUrl('https://www.misturnos.somee.com/notificationHub')
-      .withAutomaticReconnect()
-      .build();
+    useEffect(() => {
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("/notificationHub", {
+                accessTokenFactory: () => token
+            })
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
 
-    connection.start()
-      .then(() => {
-        console.log('Connected to SignalR hub');
-      })
-      .catch(error => console.error('SignalR Connection Error: ', error));
+        connection.start()
+            .then(() => console.log('Connected to SignalR'))
+            .catch(err => console.error('SignalR Connection Error: ', err));
 
-    connection.on('ReceiveNotification', (message) => {
-      setNotifications(prev => [...prev, message]);
-    });
+        connection.on("ReceiveNotification", (message) => {
+            setMessages(prevMessages => [...prevMessages, message]);
+        });
 
-    return () => {
-      connection.stop();
-    };
-  }, []);
+        return () => {
+            connection.stop().then(() => console.log('Disconnected from SignalR'));
+        };
+    }, [token]);
 
-  return (
-    <div>
-      <h2>Notifications</h2>
-      <ul>
-        {notifications.map((notif, index) => (
-          <li key={index}>{notif}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Notificaciones:</h2>
+            <ul>
+                {messages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Notifications;
