71289bf22ffc5433da3d392ebf51dcb1661db464
Santi-Teran <santiagoteranmatias@gmail.com>
2024-08-29 16:29:24 -0300

    fix

--- src/components/dashboard/BusinessLinks.jsx
+++ src/components/dashboard/BusinessLinks.jsx
@@ -11,7 +11,6 @@ const BusinessLinks = () => {
       <Link className={isActive('/dashboard/negocio') ? 'text-cyan-500 font-bold border-b-2 border-cyan-500' : 'text-gray-400'} href='/dashboard/negocio'>General</Link>
       <Link className={isActive('/dashboard/negocio/turnos') ? 'text-cyan-500 font-bold border-b-2 border-cyan-500' : 'text-gray-400'} href='/dashboard/negocio/turnos'>Turnos</Link>
       <Link className={isActive('/dashboard/negocio/preferencias') ? 'text-cyan-500 font-bold border-b-2 border-cyan-500' : 'text-gray-400'} href='/dashboard/negocio/preferencias'>Preferencias</Link>
-      <Link className={isActive('/dashboard/negocio/perfil') ? 'text-cyan-500 font-bold border-b-2 border-cyan-500' : 'text-gray-400'} href='/dashboard/negocio/perfil'>Perfil</Link>
     </ul>
   )
 }
