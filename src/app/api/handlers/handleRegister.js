47d23cabec56417db4e4166e233f7bf58f23afbb
Santi-Teran <santiagoteranmatias@gmail.com>
2024-09-05 10:56:25 -0300

    fix register

--- src/app/api/handlers/handleRegister.js
+++ src/app/api/handlers/handleRegister.js
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { registerUser, verifyCode } from '../api';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { registerUser, verifyCode } from "../api";
 
 export const useRegister = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    address: '',
-    password: '',
-    subscriptionId: '',
+    name: "",
+    email: "",
+    userConfiguration: {
+      address: {
+        addressLine: "",
+      },
+    },
+    password: "",
+    subscriptionId: "",
   });
 
   const [loading, setLoading] = useState(false);
@@ -18,9 +22,9 @@ export const useRegister = () => {
 
   useEffect(() => {
     const url = window.location.href;
-    const preapprovalIdParam = url.split('preapproval_id=')[1];
+    const preapprovalIdParam = url.split("preapproval_id=")[1];
     if (preapprovalIdParam) {
-      const subscriptionId = preapprovalIdParam.split('&')[0];
+      const subscriptionId = preapprovalIdParam.split("&")[0];
       setFormData((prevData) => ({
         ...prevData,
         subscriptionId,
@@ -30,27 +34,43 @@ export const useRegister = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+
+    if (name === "addressLine") {
+      setFormData((prevData) => ({
+        ...prevData,
+        userConfiguration: {
+          ...prevData.userConfiguration,
+          address: {
+            ...prevData.userConfiguration.address,
+            addressLine: value,
+          },
+        },
+      }));
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     setError(null);
-    
+
     try {
       const result = await registerUser(formData);
       if (result.success) {
-        return true;  // Devuelve true si el registro fue exitoso
+        return true; // Devuelve true si el registro fue exitoso
       } else {
         setError(result.message);
         return false;
       }
     } catch (error) {
-      setError('Ocurrió un error al registrar tu cuenta. Inténtalo de nuevo más tarde.');
+      setError(
+        "Ocurrió un error al registrar tu cuenta. Inténtalo de nuevo más tarde."
+      );
       return false;
     } finally {
       setLoading(false);
@@ -64,12 +84,14 @@ export const useRegister = () => {
     try {
       const result = await verifyCode(formData.email, code);
       if (result.success) {
-        router.push('/login');
+        router.push("/login");
       } else {
-        setError('El código ingresado es incorrecto. Inténtalo nuevamente.');
+        setError("El código ingresado es incorrecto. Inténtalo nuevamente.");
       }
     } catch (error) {
-      setError('Ocurrió un error al verificar tu código. Inténtalo de nuevo más tarde.');
+      setError(
+        "Ocurrió un error al verificar tu código. Inténtalo de nuevo más tarde."
+      );
     } finally {
       setLoading(false);
     }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -9,10 +9,17 @@ import { useState } from "react";
 const nunito = Nunito({ subsets: ["latin"] });
 
 const RegisterBusiness = () => {
-  const { formData, handleChange, handleSubmit, handleVerifyCode, loading, error } = useRegister();
+  const {
+    formData,
+    handleChange,
+    handleSubmit,
+    handleVerifyCode,
+    loading,
+    error,
+  } = useRegister();
   const [showPassword, setShowPassword] = useState(false);
   const [verificationStep, setVerificationStep] = useState(false);
-  const [verificationCode, setVerificationCode] = useState('');
+  const [verificationCode, setVerificationCode] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -26,53 +33,117 @@ const RegisterBusiness = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={onSubmit} className="flex flex-col gap-5 p-8 rounded-lg shadow w-full max-w-md gradient">
-        <h1 className={`${nunito.className} text-3xl md:text-4xl font-black`}>{verificationStep ? 'Verificar Código' : 'Crear cuenta'}</h1>
-        
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col gap-5 p-8 rounded-lg shadow w-full max-w-md gradient"
+      >
+        <h1 className={`${nunito.className} text-3xl md:text-4xl font-black`}>
+          {verificationStep ? "Verificar Código" : "Crear cuenta"}
+        </h1>
+
         {!verificationStep ? (
           <>
             <div className="flex items-center border p-2 gap-2 rounded-lg">
               <MdPerson className="text-gray-100 text-xl" />
-              <input type="text" name="name" value={formData.name} placeholder="Nombre y Apellido" onChange={handleChange} className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100" required />
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                placeholder="Nombre y Apellido"
+                onChange={handleChange}
+                className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100"
+                required
+              />
             </div>
             <div className="flex items-center border p-2 gap-2 rounded-lg">
               <MdMail className="text-gray-100 text-xl" />
-              <input type="email" name="email" value={formData.email} placeholder="Email" onChange={handleChange} className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100" required />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                placeholder="Email"
+                onChange={handleChange}
+                className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100"
+                required
+              />
             </div>
             <div className="flex items-center border p-2 gap-2 rounded-lg">
               <MdHouse className="text-gray-100 text-xl" />
-              <input type="text" name="address" value={formData.address} placeholder="Dirección" onChange={handleChange} className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100" required />
+              <input
+                type="text"
+                name="addressLine"
+                value={formData.userConfiguration.address.addressLine}
+                placeholder="Dirección"
+                onChange={handleChange}
+                className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100"
+                required
+              />
             </div>
             <div className="flex items-center border p-2 gap-2 rounded-lg">
               <MdLock className="text-gray-100 text-xl" />
-              <input type={showPassword ? "text" : "password"} name="password" value={formData.password} placeholder="Contraseña" onChange={handleChange} className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100" required />
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={formData.password}
+                placeholder="Contraseña"
+                onChange={handleChange}
+                className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100"
+                required
+              />
               {showPassword ? (
-                <FaEyeSlash onClick={() => setShowPassword(!showPassword)} className="cursor-pointer" />
+                <FaEyeSlash
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="cursor-pointer"
+                />
               ) : (
-                <FaEye onClick={() => setShowPassword(!showPassword)} className="cursor-pointer" />
+                <FaEye
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="cursor-pointer"
+                />
               )}
             </div>
           </>
         ) : (
           <>
-            <p>Te hemos enviado un código a tu correo electrónico. Por favor, ingrésalo a continuación para verificar tu cuenta.</p>
+            <p>
+              Te hemos enviado un código a tu correo electrónico. Por favor,
+              ingrésalo a continuación para verificar tu cuenta.
+            </p>
             <div className="flex items-center border p-2 gap-2 rounded-lg">
               <MdLock className="text-gray-100 text-xl" />
-              <input type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} placeholder="Código de Verificación" className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100" required />
+              <input
+                type="text"
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
+                placeholder="Código de Verificación"
+                className="bg-transparent focus:outline-none focus:ring-0 w-full text-black placeholder:text-gray-100"
+                required
+              />
             </div>
           </>
         )}
 
-        <button type="submit" className={`${nunito.className} text-lg bg-arena text-dark-blue font-black px-6 py-2 rounded-lg`} disabled={loading}>
-          {loading ? "Cargando..." : verificationStep ? "Verificar Código" : "Registrarse"}
+        <button
+          type="submit"
+          className={`${nunito.className} text-lg bg-arena text-dark-blue font-black px-6 py-2 rounded-lg`}
+          disabled={loading}
+        >
+          {loading
+            ? "Cargando..."
+            : verificationStep
+            ? "Verificar Código"
+            : "Registrarse"}
         </button>
-        
+
         {error && <p className="text-red-500">{error}</p>}
-        
+
         {!verificationStep && (
           <p className="text-center text-sm mx-10">
             Al hacer click en este botón, acepto que he leído la{" "}
-            <Link href={"/legal"} className="underline">Política de privacidad</Link>.
+            <Link href={"/legal"} className="underline">
+              Política de privacidad
+            </Link>
+            .
           </p>
         )}
       </form>
